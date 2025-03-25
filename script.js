d8ad238aa3f023d02a6036aca10b836d60614bc9
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-03-25 11:15:14 +0000

    Add board struc and update funcinside factory

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function gameboard() {
-    gameboard = []
+    let gameboard = []
+
     for (let i = 0; i < 3; i++) {
         gameboard[i] = [];
         for (let j = 0; j < 3; j++) {
             gameboard[i][j] = 0;
         }
     }
+
+    const updateGammboard = (x, y, value) => {
+        gameboard[x][y] = value;
+        console.log(gameboard);
+    }
+
+    return {updateGammboard};
 }

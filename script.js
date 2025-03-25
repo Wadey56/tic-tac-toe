ffe356e584dab6b154f22e6eb5af041e3db9bf23
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-03-25 11:45:35 +0000

    Wrap board reset in a func

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function gameboard() {
     let gameboard = []
 
-    for (let i = 0; i < 3; i++) {
-        gameboard[i] = [];
-        for (let j = 0; j < 3; j++) {
-            gameboard[i][j] = 0;
+    const resetGameboard = () => {
+        for (let i = 0; i < 3; i++) {
+            gameboard[i] = [];
+            for (let j = 0; j < 3; j++) {
+                gameboard[i][j] = "";
+            }
         }
+        console.log(gameboard);
     }
+    
 
-    const updateGammboard = (x, y, value) => {
+    const updateGameboard = (x, y, value) => {
         gameboard[x][y] = value;
         console.log(gameboard);
     }
 
-    return {updateGammboard};
+    return {resetGameboard, updateGameboard};
 }
+
+let test = gameboard();
+test.resetGameboard();
+test.updateGameboard(2,2,"X");

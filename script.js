152cc9a897dc62ecc36fed44ebe2152fe8c9f0ee
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2025-03-25 11:53:25 +0000

    Modify names of vars

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function gameboard() {
     let gameboard = []
 
-    const resetGameboard = () => {
+    const reset = () => {
         for (let i = 0; i < 3; i++) {
             gameboard[i] = [];
             for (let j = 0; j < 3; j++) {
@@ -12,14 +12,14 @@ function gameboard() {
     }
     
 
-    const updateGameboard = (x, y, value) => {
+    const update = (x, y, value) => {
         gameboard[x][y] = value;
         console.log(gameboard);
     }
 
-    return {resetGameboard, updateGameboard};
+    return {reset, update};
 }
 
-let test = gameboard();
-test.resetGameboard();
-test.updateGameboard(2,2,"X");
+let gameboard1 = gameboard();
+gameboard1.reset();
+gameboard1.update(2,2,"X");
